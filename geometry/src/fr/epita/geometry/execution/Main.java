79c455796a73cbfc892d321507579e9f5c7df7a9
thomasbroussard <thomas.broussard@gmail.com>
2026-03-16 09:53:52 +0100

    geometry intro + interface

--- geometry/src/fr/epita/geometry/execution/Main.java
+++ geometry/src/fr/epita/geometry/execution/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(3);
 
-        Shape s = new Circle(3);
 
         double area = circle.getArea();
         System.out.println(area);
