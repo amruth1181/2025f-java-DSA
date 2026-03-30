046dcf4909eccb6f4ed2218bbd7049e8eae418fc
thomasbroussard <thomas.broussard@gmail.com>
2026-03-30 11:54:55 +0200

    introduction to db connectivity

--- biostat/src/fr/epita/biostat/services/BioStatDataService.java
+++ biostat/src/fr/epita/biostat/services/BioStatDataService.java
@@ -2,15 +2,14 @@ package fr.epita.biostat.services;
 
 import fr.epita.biostat.datamodel.BioStatEntry;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BioStatDataService {
 
 
-    private static void save(BioStatEntry entry) throws SQLException {
+    public void save(BioStatEntry entry) throws SQLException {
         try (Connection connection = getConnection()) {
             PreparedStatement insertStatement = connection.prepareStatement("""
                                 INSERT INTO BIOSTATS(name, sex, age) VALUES (?, ?, ?)
@@ -25,6 +24,60 @@ public class BioStatDataService {
             insertStatement.execute();
         }
     }
+
+
+    public List<BioStatEntry> search(BioStatEntry qbe) throws SQLException {
+        List<BioStatEntry> entries = new ArrayList<>();
+        try(Connection connection = getConnection()) {
+            String selectQuery = """
+                    SELECT name,age,sex FROM BIOSTATS 
+                    where 
+                        (? IS NULL OR name = ?) 
+                        and
+                        (? IS NULL OR age = ?)
+                        and
+                        (? IS NULL OR sex = ?)
+                    """;
+            PreparedStatement selectStatement = connection.prepareStatement(selectQuery);
+            selectStatement.setString(1, qbe.getName());
+            selectStatement.setString(2, qbe.getName());
+            selectStatement.setInt(3, qbe.getAge());
+            selectStatement.setInt(4, qbe.getAge());
+            selectStatement.setString(5, qbe.getSex());
+            selectStatement.setString(6, qbe.getSex());
+
+            ResultSet resultSet = selectStatement.executeQuery();
+
+            while(resultSet.next()){
+
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String sex = resultSet.getString("sex");
+                BioStatEntry entry = new BioStatEntry(name, sex, age, null, null);
+                entries.add(entry);
+            }
+        }
+        return entries;
+    }
+
+    public void update(BioStatEntry entry) throws SQLException {
+        try(Connection connection = getConnection()) {
+            PreparedStatement updateStatement = connection.prepareStatement("""
+                                UPDATE BIOSTATS SET age = ? where name = ?
+                """);
+
+            updateStatement.setString(2, entry.getName());
+            updateStatement.setInt(1, entry.getAge());
+            updateStatement.execute();
+        }
+    }
+
+
+    public void delete(BioStatEntry entry){
+
+    }
+
+
 
 
     private static Connection getConnection() throws SQLException {
